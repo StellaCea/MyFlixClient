546cbeaea1b2b77896d22cf17fbe9f564a485f8a
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-07-16 11:39:46 +0200

    fetch api and update props

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,26 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({movie, onMovieClick}) => {
     return(
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
+        <div>
             {movie.title}
         </div>
     );
+};
+//Define prop constraints
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.shape({
+            name: PropTypes.string,
+            bio: PropTypes.string
+        }),
+        genre: PropTypes.shape({
+            name: PropTypes.string,
+            descriprion: PropTypes.string
+        }),
+        image: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick:PropTypes.func.isRequired
 };
