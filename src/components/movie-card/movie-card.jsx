3979eadfd9d57c79505db83315c5616e3afb58a0
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-07-21 13:20:16 +0200

    try fix

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,26 +1,32 @@
+import React from "react";
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({movie, onMovieClick}) => {
+export const MovieCard = ({movie, favoriteMovies}) => {
     return(
-        <div>
-            {movie.title}
-        </div>
+        <Card className="h-100" border="primary" style={{width:"19rem"}}>
+            <Card.Img variant="top" src={movie.image} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.director.name}</Card.Text>
+                <Card.Text>{movie.description}</Card.Text>
+                <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+                    <Button variant="link">
+                        Open
+                    </Button>
+                </Link>
+            </Card.Body>
+        </Card>
     );
 };
 //Define prop constraints
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
-        description: PropTypes.shape({
-            name: PropTypes.string,
-            bio: PropTypes.string
-        }),
-        genre: PropTypes.shape({
-            name: PropTypes.string,
-            descriprion: PropTypes.string
-        }),
+        description: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
-        director: PropTypes.string.isRequired
-    }).isRequired,
-    onMovieClick:PropTypes.func.isRequired
+    }).isRequired
 };
